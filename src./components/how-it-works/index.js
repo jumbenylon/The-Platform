15a10ef9948e3d7931676afffced3aed3963fa19
jumbenylon <jumbenylon@gmail.com>
2023-05-16 14:11:35 +0300

    Delete src. directory

--- src./components/how-it-works/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./header"
-export * from "./features-section-component"
-export * from "./faq"
--- src./data/index.js
+++
@@ -1,4 +0,0 @@
-// export { MENU_FOOTER } from './menu-footer'
-export { MENU_HEADER } from './menu-header'
-export {HeaderFeatures} from './header-features'
-
